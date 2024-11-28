2761c335f9a3323e66a61dea928ec34b0e045cab
ITACHI <ajogujoseph0317@gmail.com>
2024-11-29 00:01:10 +0100

    working on hero section

--- src/components/LandingPage/Hero.jsx
+++ src/components/LandingPage/Hero.jsx
@@ -10,12 +10,13 @@ function Hero() {
     <Navbar />
     {/* circles */}
     <img className="absolute left-0 w-10/12" src={circles} alt="" />
-    <div className="w-96 h-52 left-1 top-11 bg-buttonSecondGradient absolute rounded-full blur-3xl opacity-20"></div>
-    <div className="w-96 h-52 right-1 top-96 bg-buttonSecondGradient absolute rounded-full blur-3xl opacity-20"></div>
+    
 
     <div className="flex items-center justify-center w-full px-10">
     
     <div className="w-full mt-56 flex relative justify-between z-10">
+    <div className="w-96 h-52 -left-20 -top-56 bg-buttonSecondGradient absolute rounded-full blur-3xl opacity-20"></div>
+    <div className="w-96 h-52 -right-10 top-52 bg-buttonSecondGradient absolute rounded-full blur-3xl opacity-20"></div>
     <img className="absolute left-1 z-20 -top-24" src={Star1} alt="" />
     <img className="absolute right-28 z-20 -top-36 scale-50" src={Star1} alt="" />
     <img className="absolute right-28 z-20 top-72" src={Star1} alt="" />
